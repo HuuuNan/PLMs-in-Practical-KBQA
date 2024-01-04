2b819191f76749f9b431a53eaf44004950dacef5
wuyike2000 <60560866+wuyike2000@users.noreply.github.com>
2024-01-04 12:39:02 +0800

    Update final.py

--- main/evidence_integration/final.py
+++ main/evidence_integration/final.py
@@ -9,9 +9,7 @@ from argparse import ArgumentParser
 from collections import defaultdict
 from util import clean_uri, processed_text, www2fb, rdf2fb
 
-import sys
-sys.path.append("..")
-from mylogger import mylog
+import mylog
 file_name = os.path.basename(__file__)
 file_name = file_name[0:len(file_name)-3]
 
@@ -67,17 +65,7 @@ def get_questions(filename):
         id2goldmids[lineid] = mid
     return id2questions, id2goldmids
 
-def get_mid2wiki(filename):
-    my_logger.warning("Loading Wiki")
-    mid2wiki = defaultdict(bool)
-    fin = open(filename)
-    for line in fin.readlines():
-        items = line.strip().split('\t')
-        sub = rdf2fb(clean_uri(items[0]))
-        mid2wiki[sub] = True
-    return mid2wiki
-
-def evidence_integration(data_path, ent_path, rel_path, output_dir, index_reach, index_degrees, mid2wiki, is_heuristics, HITS_ENT, HITS_REL):
+def evidence_integration(data_path, ent_path, rel_path, output_dir, index_reach, index_degrees, is_heuristics, HITS_ENT, HITS_REL):
     id2questions, id2goldmids = get_questions(data_path)
     id2mids = get_mids(ent_path, HITS_ENT)
     id2rels = get_rels(rel_path, HITS_REL)
@@ -129,7 +117,6 @@ def evidence_integration(data_path, ent_path, rel_path, output_dir, index_reach,
                         rel_score = math.exp(float(rel_log_score))
                         # comb_score = (float(mid_score)**1) + (rel_score*1000)
                         comb_score =(rel_score*1000)
-                        #id2answers[line_id].append((mid, rel, mid_name, mid_type, mid_score, rel_score, comb_score, int(mid2wiki[mid]), int(index_degrees[mid][0])))
                         if mid in index_degrees.keys():
                             de_score=index_degrees[mid][0]
                         else:
@@ -141,14 +128,7 @@ def evidence_integration(data_path, ent_path, rel_path, output_dir, index_reach,
             id2answers[line_id].sort(key=lambda t: (t[2],[3]), reverse=True)
         else:
             id2answers[line_id] = [(mids[0][0], rels[0][0])]
-        '''
-        # write to file
-        fout.write("{}".format(line_id))
-        for answer in id2answers[line_id]:
-            mid, rel, mid_name, mid_type, mid_score, rel_score, comb_score, _, _ = answer
-            fout.write(" %%%% {}\t{}\t{}\t{}\t{}\t{}".format(mid, rel, mid_name, mid_score, rel_score, comb_score))
-        fout.write('\n')
-        '''
+            
         if len(id2answers[line_id]) >= 1 and id2answers[line_id][0][0] == truth_mid \
                 and id2answers[line_id][0][1] == truth_rel:
             retrieved_top1 += 1
@@ -178,38 +158,7 @@ def evidence_integration(data_path, ent_path, rel_path, output_dir, index_reach,
             relationship_correct_top2 += 1
         elif len(id2answers[line_id]) >= 3 and id2answers[line_id][2][1] == truth_rel:
             relationship_correct_top3 += 1
-    '''
-    total=pd.read_table('../mydata/'+TYPE+'.txt', header=None, names=["lineid", "entity_mid", "entity_name", "relation", "object", "question", "tags"])  
-    top1=pd.DataFrame(columns=["lineid", "entity_mid", "entity_name", "relation", "object", "question", "tags"]) 
-    top2=pd.DataFrame(columns=["lineid", "entity_mid", "entity_name", "relation", "object", "question", "tags"]) 
-    top3=pd.DataFrame(columns=["lineid", "entity_mid", "entity_name", "relation", "object", "question", "tags"]) 
-    rest=pd.DataFrame(columns=["lineid", "entity_mid", "entity_name", "relation", "object", "question", "tags"])
-    num1=0
-    num2=0
-    num3=0
-    num4=0
-    for index, row in total.iterrows():
-        FLAG=False
-        if row['lineid'] in lineids_found1:
-            top1.loc[num1]=row
-            num1+=1
-            FLAG=True
-        if row['lineid'] in lineids_found2:
-            top2.loc[num2]=row
-            num2+=1
-            FLAG=True
-        if row['lineid'] in lineids_found3:
-            top3.loc[num3]=row
-            num3+=1
-            FLAG=True
-        if not FLAG:
-            rest.loc[num4]=row
-            num4+=1
-    top1.to_csv('./'+args.model+'_????????/'+TYPE+'/top1.txt',sep='\t',header=None,index=False)
-    top2.to_csv('./'+args.model+'_????????/'+TYPE+'/top2.txt',sep='\t',header=None,index=False)
-    top3.to_csv('./'+args.model+'_????????/'+TYPE+'/top3.txt',sep='\t',header=None,index=False)
-    rest.to_csv('./'+args.model+'_????????/'+TYPE+'/rest.txt',sep='\t',header=None,index=False)
-    '''
+            
     my_logger.warning("")
     my_logger.warning("found:              {}".format(found / len(id2goldmids) * 100.0))
     my_logger.warning("retrieved at top 1: {}".format(retrieved_top1 / len(id2goldmids) * 100.0))
@@ -219,14 +168,13 @@ def evidence_integration(data_path, ent_path, rel_path, output_dir, index_reach,
     my_logger.warning("")
     my_logger.warning(" entity correct at top 1:         {}".format(entity_correct_top1 / len(id2goldmids) * 100.0))
     my_logger.warning(" relationship correct at top 1:   {}".format(relationship_correct_top1 / len(id2goldmids) * 100.0))
-    #print("retrieved at inf:   {}".format(retrieved / len(id2goldmids) * 100.0))
-    #fout.close()
     return id2answers
 
 if __name__=="__main__":
-    TYPE = 'valid'
-    MODEL = 'albert-base-v2'
+    TYPE = 'test'
+    MODEL = 'bert-base-uncased'
     SCALE = 'small'  # small, medium1(105M), medium2(202M), large
+    DISAMB = False # flase for direct entity linking result, true for entity linking result after entity disamb
 
     # set logger
     global my_logger
@@ -261,20 +209,12 @@ if __name__=="__main__":
                         help='path to the pickle for the index with the degree counts')
     parser.add_argument('--data_path', type=str, default="../mydata/"+TYPE+".txt")
 
-    # modified by mdh
-    step_num = 2
-    my_logger.warning("step {} result :".format(step_num))
-
-    if(step_num == 2):
+    if DISAMB:
         parser.add_argument('--ent_path', type=str, default="../entity_disamb/link_result/"+SCALE+"/"+MODEL+"/"+TYPE+"-h100.txt", help='path to the entity linking results')
     else:
         parser.add_argument('--ent_path', type=str,default="../entity_linking/" + SCALE + "/" + MODEL + "/" + TYPE + "-h100.txt", help='path to the entity linking results')
 
-    # parser.add_argument('--ent_path', type=str,
-    #                     default="../entity_disamb/link_result/" + SCALE + "/" + MODEL + "/" + TYPE + "-h100.txt",
-    #                     help='path to the entity linking results')
-    parser.add_argument('--rel_path', type=str, default="../rel_prediction/"+SCALE+'/'+MODEL+'_results/'+TYPE+".txt", help='path to the relation prediction results')
-    parser.add_argument('--wiki_path', type=str, default="../data/fb2w.nt")
+    parser.add_argument('--rel_path', type=str, default="../relation_prediction/"+SCALE+'/'+MODEL+'_results/'+TYPE+".txt", help='path to the relation prediction results')
     parser.add_argument('--hits_ent', type=int, default=50, help='the hits here has to be <= the hits in entity linking')
     parser.add_argument('--hits_rel', type=int, default=5, help='the hits here has to be <= the hits in relation prediction retrieval')
     parser.add_argument('--no_heuristics', action='store_false', help='do not use heuristics', dest='heuristics')
@@ -288,16 +228,15 @@ if __name__=="__main__":
     time1=time.time()
     index_reach = load_index(args.index_reachpath)
     index_degrees = load_index(args.index_degreespath)
-    mid2wiki = get_mid2wiki(args.wiki_path)
     time2=time.time()
     process_time=time2-time1
-    my_logger.warning('??????????????{}'.format(process_time))
+    my_logger.warning('process time:{}'.format(process_time))
     
     time3=time.time()
-    test_answers = evidence_integration(args.data_path, args.ent_path, args.rel_path, output_dir, index_reach, index_degrees, mid2wiki, args.heuristics, args.hits_ent, args.hits_rel)
+    test_answers = evidence_integration(args.data_path, args.ent_path, args.rel_path, output_dir, index_reach, index_degrees, args.heuristics, args.hits_ent, args.hits_rel)
     time4=time.time()
     finalstep_time=time4-time3
     if TYPE=='valid':
-        my_logger.warning('????????????????{}'.format(finalstep_time))
+        my_logger.warning('valid time:{}'.format(finalstep_time))
     if TYPE=='test':
-        my_logger.warning('????????????????{}'.format(finalstep_time))
+        my_logger.warning('test time:{}'.format(finalstep_time))
